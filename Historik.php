b5bc9cf144c8ab71d46fc627bcfdbdd8b0cf6a28
Daniel Pilanat Østergaard <dani5905@edu.easj.dk>
2017-11-22 09:34:54 +0100

    Oprettet Index, med knapper og referencer til 1 anden side (Historik). + Stylesheet til Styling af siden

---
+++ Historik.php
@@ -0,0 +1,14 @@
+<!Doctype html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="Swag.css"
+</head>
+</html>
+<a href="Index.php">Index</a>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: danie
+ * Date: 21-11-2017
+ * Time: 13:22
+ */
