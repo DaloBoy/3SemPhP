07d5a4d538b9507e436d1d286ee1444f529535bb
Daniel Pilanat Østergaard <dani5905@edu.easj.dk>
2017-11-22 09:27:38 +0100

    Oprettet Index, med knapper og referencer til 1 anden side (Historik). + Stylesheet til Styling af siden

---
+++ Index.php
@@ -0,0 +1,18 @@
+<!Doctype html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="Swag.css"
+</head>
+</html>
+
+<h1>Målings project</h1>
+
+<p>Start eller stop måling:</p>
+<form action="welcome.php" method="post">
+    <input class="button1" type="submit" value="Start måling">
+    <input class="button2" type="submit" value="Stop måling">
+</form>
+
+<a href="Historik.php">Historik</a>
+
+<?php
