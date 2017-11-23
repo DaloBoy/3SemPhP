a49321d78210032992e1aeac3eab4035610a39bc
Daniel Pilanat Østergaard <dani5905@edu.easj.dk>
2017-11-23 11:03:35 +0100

    Fået PHP til at hente Informationer fra Databasen, og vise dem i HTML. ALDRIG BRUG "Å" I HOST-LINK

--- controllers/AllMalings.php
+++ controllers/AllMalings.php
@@ -4,32 +4,39 @@
     <link rel="stylesheet" type="text/css" href="../Swag.css"
 </head>
 </html>
-<input id="button3"
-       type="button" value="Back" onclick="window.location.href='../Index.php'">
+
 <!--<a href="Index.php">Index</a>-->
 
 
 
 
 <?php
-$uri = "http://lydmåler2.azurewebsites.net/LydLydService1.svc/lyde";
-$jsondata = file_get_contents($uri);
-//print_r($jsondata)
-$convertToArray = true;
-$maling = json_decode($jsondata, $convertToArray);
 
-print_r($maling);
+
+
+
+
+$uri = "http://lydmaler.azurewebsites.net/LydLydService1.svc/lyde/";
+$jsondata = file_get_contents($uri);
+//print_r($jsondata);
+$convertToAssociativeArray = true;
+$maling = json_decode($jsondata, $convertToAssociativeArray);
+
+//print_r($maling);
 //return
 
 require_once '../vendor/autoload.php';
 Twig_Autoloader::register();
 
-$loader = new Twig_Loader_Filesystem('../3SemPhPPart');
+$loader = new Twig_Loader_Filesystem('../views');
 $twig = new Twig_Environment($loader, array(
         //'cache' => '/path/to/compilation_cache',
         'auto_reload' => true
 ));
-$template = $twig->loadTemplate('historik.html.twig');
+$template = $twig->loadTemplate('Hist.html.twig');
 
 $parametersToTwig = array("malinger" => $maling);
 echo $template->render($parametersToTwig);
+?>
+<input id="button3"
+       type="button" value="Back" onclick="window.location.href='../Index.php'">
