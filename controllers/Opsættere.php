b1eda6d3d177584c8d5b3ee83cb4574d9f4b02a6
Daniel Pilanat Østergaard <dani5905@edu.easj.dk>
2017-12-04 12:41:05 +0100

    Stylet siderne. Mangler noget styling på Index, og skal fixe knapper på Historik/Sort Siden

--- controllers/Opsættere.php
+++ controllers/Opsættere.php
@@ -1,52 +1,76 @@
-
-<!Doctype html>
+<!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="../Swag.css"
-</head>
-</html>
+    <link rel="stylesheet" type="text/css" href="../Swag.css">
+    <style>
+        body {margin:0;}
 
-<head>
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #333;
+            position: fixed;
+            top: 0;
+            width: 100%;
+        }
+
+        li {
+            float: left;
+        }
+
+        li a {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        li a:hover:not(.active) {
+            background-color: #111;
+        }
+
+        .active {
+            background-color: blue;
+        }
+    </style>
+</head>
 <body>
-<table id="opsættere">
-    <tr>
-        <th>Navn</th>
-        <th>Telefon</th>
-        <th>Email</th>
-    </tr>
-    <tr>
-        <td>Billy Joe, Jimbob</td>
-        <td>07042017</td>
-        <td>Redneck.YEAH@gmail.com</td>
-    </tr>
-    <tr>
-        <td>Ivan Ivanson, Ivanovitch</td>
-        <td>87652341</td>
-        <td>Cyka.Blyat@Vodka.com</td>
-    </tr>
-    <tr>
-        <td>Jens Jeppersen, Jensen</td>
-        <td>55586114</td>
-        <td>Jens.Jensen@hotmail.com</td>
-    </tr>
 
-</table>
+<ul>
+    <li><a href="../controllers/Index.php">Home</a></li>
+    <li><a href="../controllers/MalingsSoap.php">Historik</a></li>
+    <li><a class="active" href="../controllers/Opsættere.php">Opsættere</a></li>
+</ul>
+
+<div style="padding:20px;margin-top:30px;background-color:lightblue;height:1500px;">
+
+    <table id="opsættere">
+        <tr>
+            <th>Navn</th>
+            <th>Telefon</th>
+            <th>Email</th>
+        </tr>
+        <tr>
+            <td>Billy Joe, Jimbob</td>
+            <td>07042017</td>
+            <td>Redneck.YEAH@gmail.com</td>
+        </tr>
+        <tr>
+            <td>Ivan Ivanson, Ivanovitch</td>
+            <td>87652341</td>
+            <td>Cyka.Blyat@Vodka.com</td>
+        </tr>
+        <tr>
+            <td>Jens Jeppersen, Jensen</td>
+            <td>55586114</td>
+            <td>Jens.Jensen@hotmail.com</td>
+        </tr>
+
+    </table>
+</div>
+
 </body>
-</head>
-<?php
-/*$peeps = array
-(
-   array("Billy Joe","Jimbob","07042017","Redneck.YEAH@gmail.com"),
-   array("Ivan Ivanson","Ivanovitch","87652341","Cyka.Blyat@Vodka.com"),
-   array("Jens Jeppersen", "Jensen","55586114","Jens.Jensen@hotmail.com"),
-);
-
-foreach($peeps as $v){
-   echo $v[1].", ".$v[0].", Telefon ". $v[2] . ", E-mail: " . $v[3].".<br>";
-}*/
-
-
-?>
-<input id="button3"
-       type="button" value="Back" onclick="window.location.href='Index.php'">
-<!--<a href="Index.php">Index</a>-->
+</html>
